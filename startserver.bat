7c2a061d1fbfae51e5a682c43742ab4e2971e0de
Chandler Hagan <gchagan36@gmail.com>
2023-09-30 18:06:29 -0400

    Removed references to ATM8

--- startserver.bat
+++ startserver.bat
@@ -1,18 +1,18 @@
 @echo off
 set FORGE_VERSION=43.2.14
-:: To use a specific Java runtime, set an environment variable named ATM8_JAVA to the full path of java.exe.
-:: To disable automatic restarts, set an environment variable named ATM8_RESTART to false.
-:: To install the pack without starting the server, set an environment variable named ATM8_INSTALL_ONLY to true.
+:: To use a specific Java runtime, set an environment variable named CCFP_JAVA to the full path of java.exe.
+:: To disable automatic restarts, set an environment variable named CCFP_RESTART to false.
+:: To install the pack without starting the server, set an environment variable named CCFP_INSTALL_ONLY to true.
 set MIRROR=https://maven.allthehosting.com/releases/
 set INSTALLER="%~dp0forge-1.19.2-%FORGE_VERSION%-installer.jar"
 set FORGE_URL="%MIRROR%net/minecraftforge/forge/1.19.2-%FORGE_VERSION%/forge-1.19.2-%FORGE_VERSION%-installer.jar"
 
 :JAVA
-if not defined ATM8_JAVA (
-    set ATM8_JAVA=java
+if not defined CCFP_JAVA (
+    set CCFP_JAVA=java
 )
 
-"%ATM8_JAVA%" -version 1>nul 2>nul || (
+"%CCFP_JAVA%" -version 1>nul 2>nul || (
    echo Minecraft 1.19 requires Java 17 - Java not found
    pause
    exit /b 1
@@ -29,23 +29,23 @@ if not exist "libraries" (
     )
     
     echo Running Forge installer.
-    "%ATM8_JAVA%" -jar %INSTALLER% -installServer -mirror %MIRROR%
+    "%CCFP_JAVA%" -jar %INSTALLER% -installServer -mirror %MIRROR%
 )
 
 if not exist "server.properties" (
     (
         echo allow-flight=true
-        echo motd=All the Mods 8
+        echo motd=C&C's Funpack
         echo max-tick-time=180000
     )> "server.properties"
 )
 
-if "%ATM8_INSTALL_ONLY%" == "true" (
+if "%CCFP_INSTALL_ONLY%" == "true" (
     echo INSTALL_ONLY: complete
     goto:EOF
 )
 
-for /f tokens^=2-5^ delims^=.-_^" %%j in ('"%ATM8_JAVA%" -fullversion 2^>^&1') do set "jver=%%j"
+for /f tokens^=2-5^ delims^=.-_^" %%j in ('"%CCFP_JAVA%" -fullversion 2^>^&1') do set "jver=%%j"
 if not %jver% geq 17  (
     echo Minecraft 1.19 requires Java 17 - found Java %jver%
     pause
@@ -53,9 +53,9 @@ if not %jver% geq 17  (
 ) 
 
 :START
-"%ATM8_JAVA%" @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-%FORGE_VERSION%/win_args.txt nogui
+"%CCFP_JAVA%" @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-%FORGE_VERSION%/win_args.txt nogui
 
-if "%ATM8_RESTART%" == "false" ( 
+if "%CCFP_RESTART%" == "false" ( 
     goto:EOF 
 )
 
